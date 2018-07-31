b81669b7a0dc5ab2c73afd024b2ad440644ad14f
YANBIN <yanbin.hung@eyesmedia.tw>
2018-07-31 17:25:28 +0800

    Wrap code block with try-catch it will crash without it, weird possible answer : https://stackoverflow.com/questions/40618803/android-app-crashes-when-launched-in-debug-mode

--- app/src/main/java/yanbin/com/textpicker/TypeFaceHelper.kt
+++ app/src/main/java/yanbin/com/textpicker/TypeFaceHelper.kt
@@ -7,12 +7,15 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.support.v4.provider.FontRequest
 import android.support.v4.provider.FontsContractCompat
+import android.util.Log
 import android.util.LruCache
 import android.widget.TextView
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 
 private const val CACHE_SIZE = 20
 
-class TypeFaceHelper(private val context: Context){
+class TypeFaceHelper(private val context: Context) {
 
     private val requestThread = HandlerThread("fontRequestThread")
     private val typeFaceCache = LruCache<String, Typeface?>(CACHE_SIZE)
@@ -23,41 +26,48 @@ class TypeFaceHelper(private val context: Context){
         handler = Handler(requestThread.looper)
     }
 
-    fun setTypeFace(textView: TextView, font: String, onRetrived: () -> Unit = {}){
-        if(typeFaceCache[font] != null){
-            textView.typeface = typeFaceCache[font]
-            textView.setTextColor(Color.BLACK)
-            onRetrived()
-            return
-        }
-
-        val fontRequest = FontRequest("com.google.android.gms.fonts",
-                "com.google.android.gms",
-                "name=$font",
-                R.array.com_google_android_gms_fonts_certs)
-
-        //TODO lifecycle!!
-        val fontCallback = object : FontsContractCompat.FontRequestCallback() {
-            override fun onTypefaceRetrieved(typeface: Typeface?) {
-                if(textView.text == font){
-                    textView.typeface = typeface
-                    textView.setTextColor(Color.BLACK)
-                    onRetrived()
-                }
-                typeFaceCache.put(font, typeface)
+    fun setTypeFace(textView: TextView, font: String, onRetrived: () -> Unit = {}) {
+        try {
+            if (typeFaceCache[font] != null) {
+                textView.typeface = typeFaceCache[font]
+                textView.setTextColor(Color.BLACK)
+                onRetrived()
+                return
             }
 
-            override fun onTypefaceRequestFailed(reason: Int) {
-                super.onTypefaceRequestFailed(reason)
-                if(textView.text == font){
-                    textView.typeface = Typeface.DEFAULT
-                    textView.setText(R.string.error_font_not_found)
-                    textView.setTextColor(Color.RED)
-                    onRetrived()
+            val fontRequest = FontRequest("com.google.android.gms.fonts",
+                    "com.google.android.gms",
+                    "name=$font",
+                    R.array.com_google_android_gms_fonts_certs)
+
+            //TODO lifecycle!!
+            val fontCallback = object : FontsContractCompat.FontRequestCallback() {
+                override fun onTypefaceRetrieved(typeface: Typeface?) {
+                    if (textView.text == font) {
+                        textView.typeface = typeface
+                        textView.setTextColor(Color.BLACK)
+                        onRetrived()
+                    }
+                    typeFaceCache.put(font, typeface)
+                }
+
+                override fun onTypefaceRequestFailed(reason: Int) {
+                    super.onTypefaceRequestFailed(reason)
+                    if (textView.text == font) {
+                        textView.typeface = Typeface.DEFAULT
+                        textView.setText(R.string.error_font_not_found)
+                        textView.setTextColor(Color.RED)
+                        onRetrived()
+                    }
                 }
             }
-        }
 
-        FontsContractCompat.requestFont(context, fontRequest, fontCallback, handler)
+
+            launch(UI) {
+                FontsContractCompat.requestFont(this@TypeFaceHelper.context, fontRequest, fontCallback, handler)
+            }
+        } catch (e: Exception) {
+            Log.e("TypeFaceHelper", "error", e)
+        }
     }
 }
